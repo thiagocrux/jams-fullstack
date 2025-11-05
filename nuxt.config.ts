88c46efee355447d92757f74f19ffca1fdd97309
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-11-05 10:50:20 -0300

    chore: config tailwindcss and root folder alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
+  vite: {
+    plugins: [viteTsconfigPaths(), tailwindcss()],
+  },
+  alias: {
+    '@': fileURLToPath(new URL('./', import.meta.url)),
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './app/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './features/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
